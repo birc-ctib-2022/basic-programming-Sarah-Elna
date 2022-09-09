85ee324cd12f642f557ccd8040ee06ead6547e60
Sara-Elna <elna.elisa@gmail.com>
2022-09-09 08:23:01 +0200

    The absoluteBEST way to create a pattern created

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,13 @@
 
 # Print the pattern
+
+a = ["*"]
+b = ["*", "*"]
+c = ["*", "*", "*"]
+d = ["*", "*", "*", "*"]
+e = ["*", "*", "*", "*", "*"]
+
+pattern_list = [a, b, c, d, e, d, c, b, a]
+
+for lst in pattern_list:
+    print(*lst)
